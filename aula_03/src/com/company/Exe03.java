2a108c7a87e6e34698826dedac96d736769103ce
ramonsl <ramonsl@gmail.com>
2020-08-21 20:33:25 -0300

    Exercicio 03
    Exercicio 03

--- aula_03/src/com/company/Exe03.java
+++ aula_03/src/com/company/Exe03.java
@@ -1,4 +1,32 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Exe03 {
+/*
+Usando a classe JOptionPane para entrada de dados,
+   elabore uma classe que receba a nota de duas provas e
+    de um trabalho. Calcule e mostre a média e o resultado
+    final (aprovado ou reprovado). Para calcular a média utilize a fórmula
+    seguinte: média = (nota da prova 1 + nota da prova 2 + nota do trabalho) / 3.
+        Considere que a média mínima para aprovação é 7.
+ */
+
+    public static void main(String[] args) {
+        double nota01,nota02,nota03, media;
+        /*
+        String input =JOptionPane.showInputDialog("Nota de n1");
+        n1 =Double.parseDouble(input);
+         */
+        nota01= Double.parseDouble(JOptionPane.showInputDialog("Nota de n1"));
+        nota02= Double.parseDouble(JOptionPane.showInputDialog("Nota de n2"));
+        nota03= Double.parseDouble(JOptionPane.showInputDialog("Nota de T1"));
+        media = (nota01+nota02+nota03)/3;
+        if(media>7){
+            System.out.println("Uhuuuuuuu aprovado");
+        }else{
+            System.out.println("errou");
+        }
+    }
+
 }
